62e4cf049e56bf150a0f8be714b1ceeac3c90334
stanley <stanleyhalim.work@gmail.com>
2021-12-13 20:45:36 +0700

    Change requirement

--- mudah_coding/settings.py
+++ mudah_coding/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-12mqhjyj1n@2@hcy0z%u+zrmygo#qm*nk0^5vwmfg567isxi=$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.213','localhost']
+ALLOWED_HOSTS = ['192.168.1.213','localhost','127.0.0.1']
 
 
 # Application definition
